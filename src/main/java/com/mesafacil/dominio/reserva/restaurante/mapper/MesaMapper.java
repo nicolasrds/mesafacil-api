7bf9aa28e53a65df3e45fcc8a0af0a626a12a665
Gabriel Câmara <gabrielzc88@gmail.com>
2024-09-19 17:32:42 -0300

    Adicionei arquivos Mesa, MesaDTO, MesaMapper, Mesa Repository;

--- src/main/java/com/mesafacil/dominio/reserva/restaurante/mapper/MesaMapper.java
+++ src/main/java/com/mesafacil/dominio/reserva/restaurante/mapper/MesaMapper.java
@@ -2,15 +2,18 @@ package com.mesafacil.dominio.reserva.restaurante.mapper;
 
 import com.mesafacil.dominio.reserva.restaurante.entity.MesaDto;
 import com.mesafacil.dominio.reserva.restaurante.model.Mesa;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
 
-import java.util.List;
-
+@Mapper
 public interface MesaMapper {
 
+    MesaMapper INSTANCE = Mappers.getMapper(MesaMapper.class);
+
+    @Mapping(source = "idRestaurante", target = "restaurante.id")
     Mesa dtoToEntity(MesaDto dto);
-
-    MesaDto entityToDto(Mesa entity);
-
-    List<MesaDto> toDtoList(List<Mesa> mesas);
+    @Mapping(source = "restaurante.id", target = "idRestaurante")
+    MesaDto entityToDto(Mesa mesa);
 
 }
